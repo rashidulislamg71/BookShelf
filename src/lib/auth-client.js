d374eaa4af9ff9169302e0d7bb753422e986e31d
rashidulislamg71 <jrashidulislam0103@gmail.com>
2026-05-11 06:01:10 +0600

    change base url

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -2,6 +2,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: process.env.NEXT_PUBLIC_AUTH_BASE_URL || "https://bookshelf-seven-pearl.vercel.app",
 })
 export const { signIn, signUp, signOut, useSession } = createAuthClient()
